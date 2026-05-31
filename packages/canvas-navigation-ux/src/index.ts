702d37d112af7e1945c17ea8bb4340a8efd2ef6a
TreptowerPark <djbriancares@gmail.com>
2026-05-31 19:19:33 +0200

    Add canvas navigation UX kit

---
+++ packages/canvas-navigation-ux/src/index.ts
@@ -0,0 +1,5 @@
+export * from './wheelIntentPolicy.js';
+export * from './navigationBallistics.js';
+export * from './safeAreaViewport.js';
+export * from './focusCameraPolicy.js';
+export * from './spatialHotkeys.js';
